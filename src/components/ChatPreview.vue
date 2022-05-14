651ed6f060ea73f90e5f6ef60b36c2d113ec1dab
mth1lda <91593194+mth1lda@users.noreply.github.com>
2022-05-14 20:04:15 +0500

    chat list design

---
+++ src/components/ChatPreview.vue
@@ -0,0 +1,29 @@
+<template>
+    <v-list-item
+        ripple
+    >
+        <v-list-item-avatar>
+        <img :src="avatar">
+        </v-list-item-avatar>
+        <v-list-item-content>
+        <v-list-item-title v-html="title"></v-list-item-title>
+        <v-list-item-subtitle
+            v-html="subtitle"
+        ></v-list-item-subtitle>
+        </v-list-item-content>
+    </v-list-item>
+</template>
+
+<script>
+  export default {
+    name: "chat-preview",
+    data: () => ({
+     
+    }),
+    props: {
+        avatar: {type: String, default: require('../assets/avatar.png')},
+        title: {type: String, default: 'первый чатик'},
+        subtitle: {type: String, default: 'приветик'}
+    }
+  }
+</script>
--- src/views/ChatsView.vue
+++ src/views/ChatsView.vue
@@ -1,5 +1,33 @@
 <template>
   <div class="chats">
     <h1>а тут много чатиков</h1>
+    <v-list two-line>
+        <chat-preview :title="'другой чатик'"/>
+        <chat-preview />
+        <chat-preview />
+        <chat-preview />
+        <chat-preview />
+        <chat-preview />
+        <chat-preview />
+        <chat-preview />
+        <chat-preview />
+        <chat-preview />
+        <chat-preview />
+        <chat-preview />
+        <chat-preview />
+        <chat-preview />
+        <chat-preview />
+    </v-list>
   </div>
 </template>
+
+<script>
+import ChatPreview from '../components/ChatPreview.vue'
+
+export default {
+    name: "chats-view",
+    components: {
+        ChatPreview
+    }
+  }
+</script>
