68be3788c9f0068bbb7e5379ce48effb5a61b2b0
mth1lda <91593194+mth1lda@users.noreply.github.com>
2022-05-14 17:56:32 +0500

    Initial commit

---
+++ src/App.vue
@@ -0,0 +1,22 @@
+<template>
+  <v-app>
+    <top-toolbar />
+    <v-main>
+      <router-view/>
+    </v-main>
+  </v-app>
+</template>
+
+<script>
+import TopToolbar from './components/TopToolbar.vue'
+
+export default {
+  name: 'App',
+  components: {
+    TopToolbar
+  },
+  data: () => ({
+    //
+  }),
+};
+</script>
---
+++ src/components/TopToolbar.vue
@@ -0,0 +1,64 @@
+<template>
+  <div>
+
+    <v-navigation-drawer
+      app
+      fixed
+      v-model="TopToolbarClick"
+
+    >
+      <v-list dense>
+        <v-list-item>
+          <v-list-item-action>
+            <v-icon>settings</v-icon>
+          </v-list-item-action>
+          <v-list-item-content>
+            <v-list-item-title>Настройки</v-list-item-title>
+          </v-list-item-content>
+        </v-list-item>
+
+        <v-list-item>
+          <v-list-item-action>
+            <v-icon>help</v-icon>
+          </v-list-item-action>
+          <v-list-item-content>
+            <v-list-item-title>Помощь</v-list-item-title>
+          </v-list-item-content>
+        </v-list-item>
+
+        <v-list-item>
+          <v-list-item-action>
+            <v-icon>crop_portrait</v-icon>
+          </v-list-item-action>
+          <v-list-item-content>
+            <v-list-item-title>выход</v-list-item-title>
+          </v-list-item-content>
+        </v-list-item>
+
+      </v-list>
+    </v-navigation-drawer>
+
+    <v-app-bar
+      app
+      fixed
+      dark
+      color="#4672B0"
+    >
+      <v-app-bar-nav-icon @click="TopToolbarClick=!TopToolbarClick"></v-app-bar-nav-icon>
+
+      <v-toolbar-title>Чаты</v-toolbar-title>
+
+    </v-app-bar>
+
+  </div>
+</template>
+
+<script>
+  export default {
+    name: 'top-toolbar',
+    data: () => ({
+        TopToolbarClick: false
+    })
+  }
+
+</script>
